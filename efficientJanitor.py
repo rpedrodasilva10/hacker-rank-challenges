c9929a9445201635c8f5ffe818d1db002d02843f
Renan Pedro da Silva <rpedrodasilva10@gmail.com>
2020-02-15 23:03:24 -0300

    Remove print

--- efficientJanitor.py
+++ efficientJanitor.py
@@ -1,18 +1,16 @@
-
-
 def efficientJanitor(weight):
-    dp = [1] * len(weight)
-    currWeightSum = weight[0]
+    trips = [1] * len(weight)
+    weight_sum = weight[0]
 
-    for i in range (1, len(dp)):
-        if (currWeightSum + weight[i] > 3):
-            dp[i] = dp[i - 1] + 1
-            currWeightSum = weight[i]
+    for i in range (1, len(trips)):
+        if (weight_sum + weight[i] > 3):
+            trips[i] = trips[i - 1] + 1
+            weight_sum = weight[i]
         else:
-            currWeightSum += weight[i]
-            dp[i] = dp[i - 1]
+            weight_sum += weight[i]
+            trips[i] = trips[i - 1]
     
-    print(dp[len(dp) - 1])
+    return (trips[len(trips) - 1])
 
-arr = [2.99, 3.0,3.0]
-efficientJanitor(arr)
+weights = [2.99,1,1, 1,  3.0,3.0]
+efficientJanitor(weights)
