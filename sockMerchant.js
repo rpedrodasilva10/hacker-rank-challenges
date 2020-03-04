f14ebc6fedbe1512d66e1cfe539938791e7f9e31
Renan Pedro da Silva (STAR SOFT) <rpedrodasilva10@gmail.com>
2020-03-04 16:05:45 -0300

    refact: remove duplicated method and switch loop with map

--- sockMerchant.js
+++ sockMerchant.js
@@ -1,43 +1,4 @@
-
 function sockMerchant(n, socksArray) {
-  //socksArray.reduce(sock => socksArray.filter())
-  let count = 0
-  const counted = []
-  let sum = 0
-
-  for (let i = 0; i < socksArray.length; i++) {
-    count = 0
-    if (!(counted.find(sock => sock == socksArray[i]))) {
-      for (let j = 0; j < socksArray.length; j++) {
-        if (socksArray[j] == socksArray[i]) {
-          count += 1
-        }
-      }
-
-      let resp = count % 2 == 0 ? count / 2 : Math.floor(count / 2)
-      // console.log(`elemento: ${socksArray[i]} Quantidade: ${count} Pares: ${resp}`)
-
-      counted.push(socksArray[i])
-      sum += resp
-
-    }
-
-  }
-
-  console.log(sum)
-  return sum
-}
-
-
-const socksArray = [10, 20, 20, 10, 10, 30, 50, 10, 20]
-
-sockMerchant(9, socksArray)
-sockMerchant2(9, socksArray)
-
-
-
-function sockMerchant2(n, socksArray) {
-  //socksArray.reduce(sock => socksArray.filter())
   let count = 0
   const counted = []
   let sum = 0
@@ -62,3 +23,9 @@ function sockMerchant2(n, socksArray) {
   })
   return sum
 }
+
+
+const socksArray = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+
+sockMerchant(9, socksArray)
+
